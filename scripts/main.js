d29651cf7768fee2dbaf25d6784168e33c88c1bf
RyanzpLee <lzpryan@gmail.com>
2020-10-15 18:17:47 +0100

    Hover effect added; does not work after grid reset

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,33 @@
-const grid = document.querySelector(".grid_container")
-const createBtn = document.querySelector(".create_btn")
-const resetBtn = document.querySelector(".reset_btn")
+const grid = document.querySelector(".grid_container");
+const resetBtn = document.querySelector(".reset_btn");
 
+// Inital grid on page load
+window.onload = createGrid(16);
 
-// On click, populate grid with tiles
-createBtn.addEventListener("click", createGrid());
+// Reset grid on click
+resetBtn.addEventListener("click", () => {
+    grid.innerHTML = '';
+    createGrid(16);
+})
+
 
 // Create the tiles inside the grid container
-function createGrid() {
-
+function createGrid(rows) {
+    console.log("created")
     // loop to create the tiles and append to the container
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < rows; j++) {
             let div = document.createElement('div');
             div.className = 'grid_tile';
             grid.appendChild(div);
-        }
-        
+        }   
     }
-
 };
+
+
+// Hover over tiles to change colour
+document.querySelectorAll('.grid_tile').forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "black";
+    });
+})
