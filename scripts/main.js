81fbb38f3fc0c61d1a551f52314973ec02e733fd
RyanzpLee <lzpryan@gmail.com>
2020-10-15 19:41:00 +0100

    Colour change, and grid size change functions added

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,55 @@
-const grid = document.querySelector(".grid_container");
-const resetBtn = document.querySelector(".reset_btn");
+const grid = document.querySelector('.grid_container');
+const reset = document.querySelector('.reset_btn');
+const slider = document.querySelector('#slider');
+const sliderValue = document.querySelector('#sliderValue');
+const gridHeight = grid.clientHeight;
+
+let colour = '#000000';
+let rows = 16;
 
 // Inital grid on page load
-window.onload = createGrid(16);
+window.onload = createGrid();
+
+function setColour(col) {
+  colour = col;
+}
+
+slider.addEventListener('input', () => {
+  grid.innerHTML = '';
+  rows = slider.value;
+  sliderValue.textContent = slider.value;
+  createGrid();
+});
+
+function displaySliderValue() {}
 
 // Reset grid on click
-resetBtn.addEventListener("click", () => {
-    grid.innerHTML = '';
-    createGrid(16);
-})
-
+reset.addEventListener('click', () => {
+  grid.innerHTML = '';
+  rows = 16
+  createGrid();
+});
 
 // Create the tiles inside the grid container
-function createGrid(rows) {
-    console.log("created")
-    // loop to create the tiles and append to the container
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < rows; j++) {
-            let div = document.createElement('div');
-            div.className = 'grid_tile';
-            grid.appendChild(div);
-        }   
+function createGrid() {
+  let tileHeight = gridHeight / rows - 2 + 'px';
+  let tileWidth = tileHeight;
+
+  // loop to create the tiles and append to the container
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < rows; j++) {
+      let div = document.createElement('div');
+      div.className = 'grid_tile';
+      div.style.height = tileHeight;
+      div.style.width = tileWidth;
+      grid.appendChild(div);
     }
-};
+  }
 
-
-// Hover over tiles to change colour
-document.querySelectorAll('.grid_tile').forEach(item => {
-    item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "black";
+  // Hover over tiles to change colour
+  document.querySelectorAll('.grid_tile').forEach((item) => {
+    item.addEventListener('mouseover', () => {
+      item.style.backgroundColor = colour;
     });
-})
+  });
+}
